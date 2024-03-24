42a77c2f4cef6c2794a57ca43f861bdc823bf031
Madhav051 <guptamadhav1729@gmail.com>
2024-03-25 02:15:17 +0530

    why react-router-dom

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -10,6 +10,15 @@ import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
+// the reason we are using react router dom is that if we have multiple pages
+// then on if we link all that pages using anchor tag then page reloads on every click
+// but this is not a good practice so react router dom gives us Link 
+// which has to similar to href in this page reload nhi hota, 
+// but in react (there is no scene of refreshing, dom jaata h 
+// and new values leke aata h (virtual dom))
+
+// react mai refresh wala toh scene hi nhi h dom jaata h and new node 
+// values add ho jaati h
 
 // const router = createBrowserRouter([
 //   {
