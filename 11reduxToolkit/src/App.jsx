0a86197594c04b08f33961661a63a298ce6e342c
shivaay2210 <guptamadhav1729@gmail.com>
2024-03-29 01:49:48 +0530

    redux-toolkit

---
+++ 11reduxToolkit/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+    <h1>
+      Learning about Redux-toolkit 
+      <AddTodo /> 
+      <Todos />
+    </h1>
+    </>
+  )
+}
+
+export default App
---
+++ 11reduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id: 1, text: "hello world"}]
+}
+
+// context api mai sirf function kaa structure define karte the definition app.jsx mai kar rhe the
+// rtk mai saara kaam yhi karna hota h
+
+export const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), 
+                // text: action.payload.text
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        }
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export const todoReducer = todoSlice.reducer
